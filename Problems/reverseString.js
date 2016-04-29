da3d746bb482d1eb7e30b75ce9ce11e9139e071d
bobbychong <bobbychong@gmail.com>
2016-04-29 14:59:01 -0700

    [problem]reversing a string

---
+++ Problems/reverseString.js
@@ -0,0 +1,13 @@
+var reverseString = function (s) {
+  var result = '';
+  for (var i = s.length - 1; i >= 0; i--) {
+    result = result + s[i];
+  }
+
+  return result;
+};
+
+// Write a function that takes a string as input and returns the string reversed.
+//
+// Example:
+// Given s = "hello", return "olleh".
